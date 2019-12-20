b05811ae3c1bf461aa736e3ddcfcc724c266f1ca
Bersh <bersh1b45@gmail.com>
2019-12-20 13:10:00 +0200

    authorization fix

--- client/main.js
+++ client/main.js
@@ -1,78 +1,70 @@
-const socket = io(); 
-let messages; 
-let isHistory = false; 
-let isVisited = false; 
+const socket = io();
+let messages;
+let isHistory = false;
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("room").onsubmit = e => {
-    e.preventDefault(); //do not update
-    const username = e.target.elements[0].value; 
-    if (!e.target.elements[0].value)
-    {
-      alert("Enter username");
-      return;
+  document.getElementById("room").onsubmit = async e => {
+    e.preventDefault();
+    console.log(await fetchUsers());
+    const username = e.target.elements[0].value;
+    if (!(await fetchUsers(username))) {
+      e.target.elements[0].value = "";
+      socket.emit("set username", username);
+      document.getElementById("username").innerHTML = username;
     }
-    e.target.elements[0].value = ""; 
-    socket.emit("set username", username); //to server
-    document.getElementById("username").innerHTML = username; 
-    isVisited = true;
   };
-
   document.getElementById("messageForm").onsubmit = e => {
-    e.preventDefault(); 
-    if (!e.target.elements[0].value)
-    {
-      alert("Dont left field empty");
-      return;
-    }    
-    if (!isVisited) {
-      alert("Firstly create account");
-      return;
-    }
-    if(elem.username == username){
-      socket.emit("message", "===" + e.target.elements[0].value); 
-    }
-    else{
-      socket.emit("message", e.target.elements[0].value); 
-    }
-    e.target.elements[0].value = ""; 
+    e.preventDefault();
+    socket.emit("message", e.target.elements[0].value);
+    e.target.elements[0].value = "";
   };
   document.getElementById("toHistory").onclick = async () => {
-    if (!isHistory && isVisited) {  
-      await fetch("/db") 
+    if (!isHistory) {
+      await fetch("/db")
         .then(response => {
           if (response.ok) {
-            return response.json(); //оbject with db data
+            return response.json();
           }
         })
         .then(data => {
-          const box = document.getElementById("messages"); 
-          messages = box.innerText; 
-          isHistory = true; 
-          box.innerText = ""; 
+          const box = document.getElementById("messages");
+          messages = box.innerText;
+          isHistory = true;
+          box.innerText = "";
           data.forEach(elem => {
-            box.innerText += `[${elem.username}]: ${elem.message}`; 
+            box.innerText += `[${elem.username}]: ${elem.message}`;
           });
         });
-    } else {
-      alert("Firstly create account");
-      return;
     }
   };
-  document.getElementById("toChat").onclick = () => { 
-    if (isHistory) { 
-      document.getElementById("messages").innerText = messages; 
-      isHistory = false; 
+  document.getElementById("toChat").onclick = () => {
+    if (isHistory) {
+      document.getElementById("messages").innerText = messages;
+      isHistory = false;
     }
   };
 });
 
+const fetchUsers = async username => {
+  const usersFetched = await fetch("/users")
+    .then(response => {
+      if (response.ok) {
+        return response.text();
+      }
+    })
+    .then(data => data.split(","));
+  return usersFetched.some(elem => elem === username);
+};
+
 socket.on("system new", name => {
-  document.getElementById("messages").innerText += `\t\t\ ${name} connected! \n`; 
+  document.getElementById("messages").innerText += `${name} joined! \n`;
 });
 
 socket.on("render message", data => {
-  document.getElementById(    
-    "messages"
-  ).innerText += `[${data.username}]: ${data.message} \n`; 
+  if(test == data.username){
+    document.getElementById("messages").innerText += `=[${data.username}]: ${data.message} \n`; 
+  }
+  else{
+    document.getElementById("messages").innerText += `[${data.username}]: ${data.message} \n`; 
+  }
 });
--- server.js
+++ server.js
@@ -1,39 +1,46 @@
-const express = require("express");
-const http = require("http");
+const express = require('express');
+const http = require('http');
+
+const users = [];
 const app = express();
 const server = http.Server(app);
 const port = process.env.PORT || 3000;
-const io = require("socket.io")(server);
-const db = require("./db.js");
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/client/index.html");
+const io = require('socket.io')(server);
+const db = require('./db.js');
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/client/index.html');
 });
 
-app.get("/db", (req, res) => { 
-  db.all("SELECT * FROM messages", (err, rows) => { 
-    res.send(rows); 
-  });
-});
+app.get('/db', (req, res) => {   
+  db.all('SELECT * FROM messages', (err, rows) => {
+  res.send(rows)
+})});
 
-app.use(express.static("./client")); //подгрузка всех файлов папки клиента
+app.get('/users', (req, res) => {
+  res.send(users.join(','));
+})
 
-io.on("connection", socket => {
-  socket.on("set username", username => {
-    socket.username = username; 
-    socket.broadcast.emit("system new", socket.username); 
-  });
-  socket.on("message", message => {
+app.use(express.static('./client'));
+
+io.on('connection', (socket) => {
+  socket.on('set username', (username) => {
+    users.push(username)
+    socket.username = username;
+    socket.broadcast.emit('system new', socket.username);
+  })
+  socket.on('message', (message) => {
     const data = {
       username: socket.username,
       message
-    };
-    db.run( 
-      `INSERT INTO messages VALUES (NULL, '${socket.username}', '${message} \n')`
-    );
-    io.emit("render message", data); 
-  });
-});
+    }
+    db.run(`INSERT INTO messages VALUES (NULL, '${socket.username}', '${message} \n')`)
+    io.emit('render message', data)
+  })
+  socket.on('disconnect', () => {
+    users.splice(users.indexOf(socket.username), 1)
+  })
+})
 
 server.listen(port, () => {
-  console.log("listening on *:" + port);
+  console.log('listening on *:' + port);
 });
